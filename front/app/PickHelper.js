3151070975f7650e7c32a06f5263512a73ea1a33
Anton Kovalenko <gugenot990@gmail.com>
2021-10-15 18:49:32 +0300

    add PickHelper

---
+++ front/app/PickHelper.js
@@ -0,0 +1,75 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.133.1';
+
+const pickPosition = { x: 0, y: 0 };
+
+class PickHelper {
+  constructor(renderer) {
+    this.raycaster = new THREE.Raycaster();
+    this.pickedObject = null;
+    this.pickedObjectSavedColor = 0;
+    this.canvas = renderer.domElement;
+
+    this.setPickPosition = this.setPickPosition.bind(this);
+    this.clearPickPosition = this.clearPickPosition.bind(this);
+    this.getCanvasRelativePosition = this.getCanvasRelativePosition.bind(this);
+
+    window.addEventListener('mousemove', this.setPickPosition);
+    window.addEventListener('mouseout', this.clearPickPosition);
+    window.addEventListener('mouseleave', this.clearPickPosition);
+
+    window.addEventListener('touchstart', (event) => {
+      // prevent the window from scrolling
+      event.preventDefault();
+      this.setPickPosition(event.touches[0]);
+    }, {passive: false});
+    
+    window.addEventListener('touchmove', (event) => {
+      this.setPickPosition(event.touches[0]);
+    });
+    
+    window.addEventListener('touchend', this.clearPickPosition);
+  }
+  pick(scene, camera) {
+    // restore the color if there is a picked object
+    if (this.pickedObject) {
+      this.pickedObject.material.color.setHex(this.pickedObjectSavedColor);
+      this.pickedObject = undefined;
+    }
+ 
+    // cast a ray through the frustum
+    this.raycaster.setFromCamera(pickPosition, camera);
+    // get the list of objects the ray intersected
+    const intersectedObjects = this.raycaster.intersectObjects(scene.children)
+      .filter(({object}) => object.material && object.material.name == 'submarine');
+      
+    if (intersectedObjects.length) {
+      // pick the first object. It's the closest one
+      this.pickedObject = intersectedObjects[0].object;
+      // save its color
+      this.pickedObjectSavedColor = this.pickedObject.material.color.getHex();
+      // set its emissive color to flashing red/yellow
+      this.pickedObject.material.color.setHex(0xFF0000);
+    }
+  }
+
+  getCanvasRelativePosition(event) {
+    const rect = this.canvas.getBoundingClientRect();
+    return {
+      x: (event.clientX - rect.left) * this.canvas.width  / rect.width,
+      y: (event.clientY - rect.top ) * this.canvas.height / rect.height,
+    };
+  }
+   
+  setPickPosition(event) {
+    const pos = this.getCanvasRelativePosition(event);
+    pickPosition.x = (pos.x / this.canvas.width ) *  2 - 1;
+    pickPosition.y = (pos.y / this.canvas.height) * -2 + 1;
+  }
+
+  clearPickPosition() {
+    pickPosition.x = -100000;
+    pickPosition.y = -100000;
+  }
+}
+
+export default PickHelper;
--- front/app/index.js
+++ front/app/index.js
@@ -4,14 +4,21 @@ import Stats from 'https://cdn.skypack.dev/three/examples/jsm/libs/stats.module.
 import { FirstPersonControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/FirstPersonControls.js';
 import { ImprovedNoise } from 'https://cdn.skypack.dev/three/examples/jsm/math/ImprovedNoise.js';
 import { OBJLoader } from 'https://cdn.skypack.dev/three/examples/jsm/loaders/OBJLoader.js';
+import PickHelper from './PickHelper.js';
 
 let container, stats;
-let camera, controls, scene, renderer;
+let camera, controls, scene, renderer, pickHelper;
 let mesh, texture;
 
 const worldWidth = 512, worldDepth = 512;
 const clock = new THREE.Clock();
-const SUBMARINES_DATA = [{ x: 1000, y: 1000, z: 1000 }, { x: 1500, y: 800, z: 1000 }, { x: 500, y: 1800, z: 4000 }];
+const SUBMARINES_DATA = [
+    { x: 1000, y: 1000, z: 1000, direction: Math.PI / 2, size: 1 },
+    { x: 1500, y: 800, z: -2000, direction: Math.PI / 4, size: 2  },
+    { x: 500, y: 1800, z: 4000, direction: Math.PI / 6, size: 3  },
+    { x: -2500, y: 1000, z: 4000, direction: Math.PI * 1.5, size: 1  },
+    { x: 3500, y: 1000, z: -1000, direction: Math.PI * 2, size: 2  }
+];
 
 init();
 animate();
@@ -20,7 +27,7 @@ function init() {
 
     container = document.getElementById( 'WebGL-output' );
 
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 50000 );
 
     scene = new THREE.Scene();
     scene.fog = new THREE.FogExp2( 0x5dc7fc, 0.0001 );
@@ -28,7 +35,7 @@ function init() {
 
     const data = generateHeight( worldWidth, worldDepth );
 
-    camera.position.set( - 1000, 1000, - 1400 );
+    camera.position.set( -1000, 1000, -1400 );
     camera.lookAt( 100, 1010, 2300 );
 
     const geometry = new THREE.PlaneGeometry( 15000, 15000, worldWidth - 1, worldDepth - 1 );
@@ -58,37 +65,43 @@ function init() {
     controls.movementSpeed = 850;
     controls.lookSpeed = 0.1;
 
-    SUBMARINES_DATA.forEach(sub => addSubmarine({ x: sub.x, y: sub.y, z: sub.z }))
+    SUBMARINES_DATA.forEach(sub => addSubmarine({ x: sub.x, y: sub.y, z: sub.z, direction: sub.direction, size: sub.size }));
 
     stats = new Stats();
     container.appendChild( stats.dom );
 
     window.addEventListener('resize', onWindowResize );
+
+    pickHelper = new PickHelper(renderer);
 }
 
-function addSubmarine({ x, y, z }) {
+function addSubmarine({ x, y, z, direction, size }) {
     const objLoader = new OBJLoader();
     objLoader.load('/models/submarine/uploads_files_989493_submarine.obj', (root) => {
-        const arrowHelper = new THREE.ArrowHelper( new THREE.Vector3( x, y, z ), new THREE.Vector3( x, 0, z), 20000, 0x48ff00, 0, 0 );
-        scene.add( arrowHelper );
+        const verticalArrowHelper = new THREE.ArrowHelper( new THREE.Vector3( x, y, z ), new THREE.Vector3( x, 0, z), 20000, 0x48ff00, 0, 0 );
+        const directionArrowHelper = new THREE.ArrowHelper( new THREE.Vector3( x, y, z ), new THREE.Vector3( x, y, z), 1400, 0xeb4034 );
+        directionArrowHelper.rotateY(direction);
+        directionArrowHelper.rotateX(Math.PI / 2);
+        scene.add( verticalArrowHelper );
+        scene.add( directionArrowHelper );
 
-        const material = new THREE.MeshBasicMaterial({ color: 0x000000, opacity: 0.4 });
+        const material = new THREE.MeshBasicMaterial({ color: 0x000000, opacity: 0.4, name: 'submarine' });
         root.traverse(node => node.material = material);
         scene.add(root);
+
+        root.scale.set( 20 * size, 20 * size, 20 * size );
         root.position.set(x, y, z);
-        root.scale.set( 20, 20, 20 );
+        root.rotateY(direction);
     });
 }
 
 function onWindowResize() {
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
 
     controls.handleResize();
-
 }
 
 function generateHeight( width, height ) {
@@ -124,15 +137,14 @@ function generateHeight( width, height ) {
 }
 
 function generateTexture( data, width, height ) {
-
     let context, image, imageData, shade;
 
     const vector3 = new THREE.Vector3( 0, 0, 0 );
+    const canvas = document.createElement( 'canvas' );
 
     const sun = new THREE.Vector3( 1, 1, 1 );
     sun.normalize();
 
-    const canvas = document.createElement( 'canvas' );
     canvas.width = width;
     canvas.height = height;
 
@@ -143,7 +155,6 @@ function generateTexture( data, width, height ) {
     imageData = image.data;
 
     for ( let i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
-
         vector3.x = data[ j - 2 ] - data[ j + 2 ];
         vector3.y = 2;
         vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
@@ -154,12 +165,9 @@ function generateTexture( data, width, height ) {
         imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
         imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
         imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-
     }
 
     context.putImageData( image, 0, 0 );
-
-    // Scaled 4x
 
     const canvasScaled = document.createElement( 'canvas' );
     canvasScaled.width = width * 4;
@@ -168,35 +176,26 @@ function generateTexture( data, width, height ) {
     context = canvasScaled.getContext( '2d' );
     context.scale( 4, 4 );
     context.drawImage( canvas, 0, 0 );
-
     image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
     imageData = image.data;
 
     for ( let i = 0, l = imageData.length; i < l; i += 4 ) {
-
         const v = ~ ~ ( Math.random() * 5 );
-
         imageData[ i ] += v;
         imageData[ i + 1 ] += v;
         imageData[ i + 2 ] += v;
-
     }
 
     context.putImageData( image, 0, 0 );
 
     return canvasScaled;
-
 }
 
-//
-
 function animate() {
-
     requestAnimationFrame( animate );
 
     render();
     stats.update();
-
 }
 
 function resizeRendererToDisplaySize(renderer) {
@@ -215,7 +214,9 @@ function render() {
         const canvas = renderer.domElement;
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
         camera.updateProjectionMatrix();
-      }
+    }
+
+    pickHelper.pick(scene, camera);
 
     controls.update( clock.getDelta() );
     renderer.render( scene, camera );
